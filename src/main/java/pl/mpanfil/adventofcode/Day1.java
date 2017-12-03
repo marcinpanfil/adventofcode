205ea2ca92ef1e3f5b746ebd3e50ea1e90858e67
marcin.panfil@gmail.com <marcin87>
2017-12-03 18:25:38 +0100

    day1

---
+++ src/main/java/pl/mpanfil/adventofcode/Day1.java
@@ -0,0 +1,29 @@
+package pl.mpanfil.adventofcode;
+
+class Day1 {
+
+    int solve1(String strToSolve) {
+        char[] chars = strToSolve.toCharArray();
+        int sum = 0;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                sum += Character.getNumericValue(chars[i]);
+            }
+        }
+        if (chars[0] == chars[chars.length - 1]) {
+            sum += Character.getNumericValue(chars[0]);
+        }
+        return sum;
+    }
+
+    int solve2(String strToSolve) {
+        char[] chars = strToSolve.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] == chars[i + chars.length/2]) {
+                sum += (2 * Character.getNumericValue(chars[i]));
+            }
+        }
+        return sum;
+    }
+}
