1b45257305ec6da19012ca31c4bfe9434d78e46b
marcin.panfil@gmail.com <marcin87>
2017-12-06 21:55:14 +0100

    day6

---
+++ src/main/java/pl/mpanfil/adventofcode/Day6.java
@@ -0,0 +1,60 @@
+package pl.mpanfil.adventofcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Day6 {
+
+    int solve1(List<Integer> numbers) {
+        int steps = 1;
+        List<String> variations = new ArrayList<>();
+
+        while (true) {
+            int max = Collections.max(numbers);
+            int index = numbers.indexOf(max);
+            numbers.set(index, 0);
+            int count = 0;
+            int current = index == numbers.size() - 1 ? 0 : index + 1;
+            distribute(numbers, max, index, count, current);
+            String varStr = numbers.toString();
+            if (variations.contains(varStr)) {
+                return steps;
+            } else {
+                variations.add(varStr);
+                steps++;
+            }
+        }
+    }
+
+    int solve2(List<Integer> numbers) {
+        List<String> variations = new ArrayList<>();
+        int steps = 1;
+        while (true) {
+            int max = Collections.max(numbers);
+            int index = numbers.indexOf(max);
+            numbers.set(index, 0);
+            int count = 0;
+            int current = index == numbers.size() - 1 ? 0 : index + 1;
+            distribute(numbers, max, index, count, current);
+            String varStr = numbers.toString();
+            if (variations.contains(varStr)) {
+                return variations.size() - variations.indexOf(varStr);
+            } else {
+                variations.add(varStr);
+            }
+        }
+    }
+
+    private void distribute(List<Integer> numbers, int max, int index, int count, int current) {
+        while (count < max) {
+            numbers.set(current, numbers.get(current) + 1);
+            count++;
+            current++;
+            if (current == numbers.size()) {
+                current = index == 0 ? 1 : 0;
+            }
+        }
+    }
+
+}
